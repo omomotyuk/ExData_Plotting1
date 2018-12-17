661e40c9ffcc5521134a2224ca9df80d1682e4c7
omomotyuk <43194659+omomotyuk@users.noreply.github.com>
2018-12-17 12:16:42 -0600

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,12 @@
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip"
+download.file( fileUrl,destfile="household_power_consumption.zip" )
+system( "unzip household_power_consumption.zip" )
+
+hpc <- read.csv( file="household_power_consumption.txt", sep=";",colClasses="character")
+hpc <- subset( hpc, Date=="1/2/2007" | Date=="2/2/2007" )
+
+png( file="plot2.png",width = 480,height = 480 )
+with(hpc, plot( strptime(paste(Date,Time), "%d/%m/%Y %H:%M:%S"), Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
+dev.off()
+
